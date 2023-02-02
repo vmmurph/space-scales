7e3d76d25b9a7d9563ee231e2a5154e3b95c4d57
vmmurph <32689160+vmmurph@users.noreply.github.com>
2023-02-02 15:39:41 -0700

    Update kerbal-data.js
    Add moons

--- kerbal-data.js
+++ kerbal-data.js
@@ -6,7 +6,7 @@ let kerbal_data = [
         color: 'white',
         density: 234.238,
         mass: 17565.459,
-        enabled: true
+        enabled: false
     },
     {
         name: 'Moho',
@@ -19,25 +19,57 @@ let kerbal_data = [
     {
         name: 'Eve',
         radius: 700,
-        color: 'purple',
+        color: 'lightpurple',
         density: 85219.677,
         mass: .12243980,
         enabled: true
     },
     {
+        name: 'Gilly',
+        radius: 13,
+        color: 'lightbrown',
+        density: 13496,
+        mass: .0000001242,
+        enabled: true
+    },
+    {
         name: 'Kerbin',
         radius: 600,
-        color: 'blue',
+        color: 'lightblue',
         density: 58484.09,
         mass: .052915,
         enabled: true
     },
     {
+        name: 'Mun',
+        radius: 200,
+        color: 'gray',
+        density: 29125,
+        mass: .00976,
+        enabled: true
+    },
+    {
+        name: 'Minmus',
+        radius: 60,
+        color: 'lightgreen',
+        density: 29242,
+        mass: .0000264,
+        enabled: true
+    },
+    {
         name: 'Duna',
         radius: 320,
-        color: 'red',
+        color: 'pink',
         density: 32897.302,
         mass: .00451542,
+        enabled: true
+    },
+    {
+        name: 'Ike',
+        radius: 130,
+        color: 'gray',
+        density: 30231.7,
+        mass: .0002782,
         enabled: true
     },
     {
@@ -54,6 +86,46 @@ let kerbal_data = [
         color: 'green',
         density: 4678.7273,
         mass: 4.2332127,
+        enabled: true
+    },
+    {
+        name: 'Laythe',
+        radius: 500,
+        color: 'lightblue',
+        density: 56144.7,
+        mass: .029397,
+        enabled: true
+    },
+    {
+        name: 'Vall',
+        radius: 300,
+        color: 'lightgreen',
+        density: 27487.5,
+        mass: .0031,
+        enabled: true
+    },
+    {
+        name: 'Tylo',
+        radius: 600,
+        color: 'lightgray',
+        density: 46787,
+        mass: .042332,
+        enabled: true
+    },
+    {
+        name: 'Bop',
+        radius: 65,
+        color: 'lightbrown',
+        density: 32391,
+        mass: .00003726,
+        enabled: true
+    },
+    {
+        name: 'Pol',
+        radius: 44,
+        color: 'lightyellow',
+        density: 30305,
+        mass: .00001081,
         enabled: true
     },
     {
